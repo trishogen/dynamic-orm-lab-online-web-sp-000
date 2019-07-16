b3f8e01b8e7c86371c4e72bdf408d166ee2e1bba
Trish Ogen <patriciaogen@gmail.com>
2019-07-16 01:20:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,7 +52,7 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-  def self.find_by(attribute:)
+  def self.find_by({attribute:})
     sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
     DB[:conn].execute(sql, attribute.key, attribute.value)
   end
