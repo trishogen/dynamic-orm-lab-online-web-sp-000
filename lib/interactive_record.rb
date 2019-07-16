e2571e4da3417d1973f697e0f1dd391b0f3618f2
Trish Ogen <patriciaogen@gmail.com>
2019-07-16 01:12:50 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,13 +38,20 @@ class InteractiveRecord
     values.join(", ")
   end
 
+  # def save
+  #   sql = <<-SQL
+  #     INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+  #     VALUES (#{values_for_insert})
+  #     SQL
+  #   DB[:conn].execute(sql)
+  #   @id = DB[:conn].execute("SELECT last_insert_rowid() from #{table_name_for_insert}")[0][0]
+  # end
+
   def save
-    sql = <<-SQL
-      INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
-      VALUES (#{values_for_insert})
-      SQL
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() from #{table_name_for_insert}")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
+
 
 end
